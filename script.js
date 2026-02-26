3f9d1d1ca4ce1e90b3b857e0b4fc6f710a7e4cff
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-02-26 17:07:26 +0530

    Create element function

--- script.js
+++ script.js
@@ -47,3 +47,23 @@ const filters = {
         unit: "%"
     }
 }
+
+function createElement(name,value,min,max,unit="%"){
+    const div= document.createElement("div");
+    div.classList.add("filter");
+    const h1= document.createElement("h1");
+
+    const input= document.createElement("input");
+    input.type= "range";
+    input.min= min;
+    input.max= max;
+    input.id= name;
+
+    const p= document.createElement("p");
+    p.innerText= name;
+
+    div.appendChild(p);
+    div.appendChild(input);
+
+    return div
+}
