26dbb06da42704fb11d2eadc150f462d57db3d74
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-02-26 16:50:24 +0530

    filter object

---
+++ script.js
@@ -0,0 +1,49 @@
+const filters = {
+    brightness: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    },
+    contrast: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }, exposure: {
+        value: 100,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }, hue_rotate: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }, blur_scale: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }, grayScale: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }, sepia: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }, opacity: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }, invert: {
+        value: 0,
+        min: 0,
+        max: 200,
+        unit: "%"
+    }
+}
